cb278ee72a64e37ced722f021d63209a4292d199
vaishakh <vaishakhsubramoain@gmail.com>
2024-11-12 07:01:21 +0000

    commit

--- LabPractice/sparse.c
+++ LabPractice/sparse.c
@@ -1,25 +1,27 @@
-#include<stdio.h>
-int main(){
-    int n,sp[10][10],tr[10][10],r,c,i,j,k=0;
-    printf("Enter the no of rows of the sparce matrix : ");
-    scanf("%d",&r);
-    printf("Enter the no of coloums of the sparce matrix : ");
-    scanf("%d",&c);
-    printf("Enter the elements of the sparse matrix : \n");
-    for(i=0;i<r;i++) {
-        for(j=0;j<c;j++){
-             scanf(" %d ",&sp[i][j]);
+#include <stdio.h>
+
+int main() {
+    int n, sp[10][10], tr[10][3], r, c, i, j, k = 1;
+    printf("Enter the no of rows of the sparse matrix: ");
+    scanf("%d", &r);
+    printf("Enter the no of columns of the sparse matrix: ");
+    scanf("%d", &c);
+    printf("Enter the elements of the sparse matrix:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            scanf("%d", &sp[i][j]);
         }
     }
-    printf("The sparse matrix entered is : \n");
-    for(i=0;i<r;i++){
-         for(j=0;j<c;j++){
-            printf(" %d ",&sp[i][j]);
-         }
+    printf("The sparse matrix entered is:\n");
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            printf("%d ", sp[i][j]);
+        }
+        printf("\n");
     }
-    for(i=0;i<r;i++){
-        for(j=0;j<c;j++){
-            if(sp[i][j] != 0){
+    for (i = 0; i < r; i++) {
+        for (j = 0; j < c; j++) {
+            if (sp[i][j] != 0) {
                 tr[k][0] = i;
                 tr[k][1] = j;
                 tr[k][2] = sp[i][j];
@@ -28,8 +30,14 @@ int main(){
         }
     }
     tr[0][0] = r;
-    tr[0][1] = j;
-    tr[0][2] = k-1;
-    printf("The tripplet representation is : \n");
-    for(i=0;i<r;i++) for(j=0;j<c;j++) printf(" %3d ",&tr[i][j]);
+    tr[0][1] = c;
+    tr[0][2] = k - 1;
+    printf("The triplet representation is:\n");
+    for (i = 0; i < k; i++) {
+        for (j = 0; j < 3; j++) {
+            printf("%3d ", tr[i][j]);
+        }
+        printf("\n");
+    }
+    return 0;
 }
